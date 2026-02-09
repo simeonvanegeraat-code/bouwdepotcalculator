238e15c182fb83fafb28e630444e2d556023e963
simeonvanegeraat-code <simeon.van.egeraat@gmail.com>
2026-02-09 01:29:22 +0100

    1

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // Dit vertelt Vite dat de root van je project DEZE map is
+  // Belangrijk: Dit vertelt Vite dat de root van je project DEZE map is
   root: '.', 
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        // Hier zeggen we: pak de index.html uit de huidige map (__dirname)
+        // Hier definiëren we de twee pagina's
         main: resolve(__dirname, 'index.html'),
         nieuwbouw: resolve(__dirname, 'nieuwbouw.html'),
       },
